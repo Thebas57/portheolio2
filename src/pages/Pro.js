a4dc774a5491fd05c76cffece96d4b89e5e2839d
Thebas57 <theohelf@gmail.com>
2023-07-25 13:08:47 +0200

    fix

--- src/pages/Pro.js
+++ src/pages/Pro.js
@@ -8,11 +8,12 @@ import Card from "../components/Card";
 
 const Pro = ({ theme, handleTheme }) => {
   const ref = useRef(null);
+  const refYy = useRef(null);
   useEffect(() => {
     // Fonction pour détecter l'événement de défilement de la souris
     function handleMouseWheel(event) {
       // Empêcher le défilement vertical par défaut
-      event.preventDefault();
+      //event.preventDefault();
 
       // Ajuster la position de défilement horizontal en fonction de la valeur de défilement de la souris
       const delta = event.deltaY;
@@ -25,12 +26,23 @@ const Pro = ({ theme, handleTheme }) => {
       }
     }
 
+    //fonction rotate yinyang
+    const rotate = () => {
+      let rot = ref.current.scrollLeft;
+      refYy.current.style.transform = `rotate(` + -rot + `deg)`;
+    };
+
     // Ajouter l'écouteur d'événement pour le défilement de la souris
-    if (window.addEventListener)
+    if (window.addEventListener) {
       // Pour les navigateurs modernes
       window.addEventListener("wheel", handleMouseWheel);
+      window.addEventListener("wheel", rotate);
+    }
 
-    return () => window.removeEventListener("wheel", handleMouseWheel);
+    return () => {
+      window.removeEventListener("wheel", handleMouseWheel);
+      window.removeEventListener("wheel", rotate);
+    };
   });
 
   return (
@@ -44,6 +56,19 @@ const Pro = ({ theme, handleTheme }) => {
       <NavLink className="back" to="/">
         <FaArrowCircleLeft />
       </NavLink>
+      <div className="yinyang-container">
+        <svg
+          aria-hidden="true"
+          data-prefix="fas"
+          data-icon="yin-yang"
+          className="prefix__svg-inline--fa prefix__fa-yin-yang prefix__fa-w-16 yinyang"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 496 512"
+          ref={refYy}
+        >
+          <path d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm0 376c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm0-128c-53.02 0-96 42.98-96 96s42.98 96 96 96c-106.04 0-192-85.96-192-192S141.96 64 248 64c53.02 0 96 42.98 96 96s-42.98 96-96 96zm0-128c-17.67 0-32 14.33-32 32s14.33 32 32 32 32-14.33 32-32-14.33-32-32-32z" />
+        </svg>
+      </div>
       <div className="pro-container pro-container-wrapper">
         <div className="card">
           <div className="front">
